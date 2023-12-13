da56242df322137d0cb3bc32440afe10371f6cd3
Jaden <jadentr.44@gmail.com>
2023-12-12 21:53:37 -0600

    adjusted mobile reponsive

--- public/script.js
+++ public/script.js
@@ -11,15 +11,15 @@ function generateBlogs(info) {
   info.forEach((blog,index)=>{
     let imgSrc = blog.imgSrc?blog.imgSrc:"https://cdn.shopify.com/s/files/1/0306/6419/6141/articles/coding_languages.png?v=1619126283"
     blogEl.innerHTML +=`
-    <div class="px-2 lg:mx-0">
+    <div class=" ">
     
    
     <a href="/${index+1}">
     <div class="w-full max-w-[25rem] mx-auto h-full shadow-lg rounded-b-lg blogCard ">
     <div style="background-image: url(${imgSrc});" class="h-[5rem] bg-cover bg-center "></div>
     <div class="p-2">
-    <h3 class="text-lg mb-auto font-semibold">${blog.title}</h3>
-    <p class="lineMax3 text-gray-500 font-light">${blog.content[0].text}</p>
+    <h3 class="md:text-lg mb-auto font-semibold">${blog.title}</h3>
+    <p class="lineMax3 md:text-base text-sm text-gray-500 font-light">${blog.content[0].text}</p>
     </div>
     </div>
     </a>
